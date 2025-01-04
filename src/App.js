08bb66baf88066c72bb8417e81713b4504ee6dba
Arunansh Srivastava <arunansh98@gmail.com>
2025-01-04 21:29:55 +0530

    Responsiveness changes wrt mobile devices

--- src/App.js
+++ src/App.js
@@ -10,14 +10,18 @@ export default function App() {
   const fetchRestaurants = () => {
     console.log({ resInput });
 
-    axios.get("/assets/data/db.json").then((response) => {
-      const items = response.data.restaurants.filter((res) =>
-        resInput && resInput !== ""
-          ? res.description.name.toLowerCase().includes(resInput.toLowerCase())
-          : true
-      );
-      setRestaurants(items);
-    });
+    axios
+      .get(`${process.env.PUBLIC_URL}/assets/data/db.json`)
+      .then((response) => {
+        const items = response.data.restaurants.filter((res) =>
+          resInput && resInput !== ""
+            ? res.description.name
+                .toLowerCase()
+                .includes(resInput.toLowerCase())
+            : true
+        );
+        setRestaurants(items);
+      });
   };
 
   useEffect(() => {
@@ -36,12 +40,12 @@ export default function App() {
         onChange={(event) => handleRestaurantInputChange(event.target.value)}
         placeholder="Enter restaurant name"
       />
-      <div className="flex items-center flex-wrap gap-[3vw] mt-[5vh] justify-center overflow-scroll">
+      <div className="flex items-center flex-wrap gap-[3vw] mt-[5vh] justify-center overflow-scroll sm:flex-row flex-col">
         {restaurants.map((restaurant) => (
           <div className="h-[40vh] w-[40vw] border-[black] border-[1px] rounded-[5px] p-[2rem] overflow-auto cursor-pointer flex flex-col items-center justify-between gap-[0.7rem]">
             <img
               className="w-[80%] h-[70%]"
-              src={restaurant.imageUrl}
+              src={`${process.env.PUBLIC_URL}${restaurant.imageUrl}`}
               alt={restaurant.id}
             />
             <h1 className="text-[1.1em]">{restaurant.description.name}</h1>
