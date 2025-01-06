c457200f71e3ce37308464d35c6c07f86c668501
Arunansh Srivastava <arunansh98@gmail.com>
2025-01-06 17:34:51 +0530

    Box Shadow Improvements

--- src/views/Restaurants.js
+++ src/views/Restaurants.js
@@ -9,7 +9,7 @@ export default function Restaurants(props) {
         <div className="mt-[4rem] text-[3rem]">No Restaurants found !</div>
       )}
       {restaurants && restaurants?.length > 0 && (
-        <div className="flex items-center flex-wrap gap-[3vw] mt-[5vh] justify-center overflow-scroll sm:flex-row flex-col">
+        <div className="flex items-center flex-wrap gap-[3vw] mt-[5vh] p-[1rem] justify-center overflow-scroll sm:flex-row flex-col">
           {restaurants.map((restaurant) => (
             <Restaurant restaurant={restaurant} />
           ))}
